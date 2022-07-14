04fd39cf7d9fb399a0d8336092ff38f0c52faed9
abhineetram <abhineet.ram@czbiohub.org>
2022-07-14 10:11:41 -0700

    Adding Laser lines

--- example.py
+++ example.py
@@ -33,10 +33,11 @@ paths = util.get_filepaths(datafolder)
 
 FPs = 'mTagBFP2','mNeonGreen','mScarlet','iRFP670' #these have to be exact matches as FPbase name
 laser_powers = [10, 10, 10, 10]
-laser_lines = ['405', '637']
+laser_lines = ['488','514','561','685'] #needs to be in quotes so can index into the csv
 exposure_times = [100, 100]
 specdata = util.get_spectra(FPs, paths, laser_lines,exposure_times)
 pdb.set_trace()
+print(specdata.keys())
 #use specdata to assemble unmixing matrix 
 
 #define experimental data
