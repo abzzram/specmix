22cfce5e143708b68bc76380ccfe99b1077757c0
abhineetram <abhineet.ram@czbiohub.org>
2022-07-21 14:53:30 -0700

    Plot em and ex spectra. fix filter pairs issues

--- popmat.py
+++ popmat.py
@@ -19,27 +19,30 @@ def populate_matrix(specdata, exc_lines, laser_powers, exposure_times):
     #assembled paired laser lines
     paired_lasers = np.zeros((len(specdata['Lambdas']),len(exc_lines)))
     adjusted_lasers = specdata['lasers'] * laser_powers
-    # plt.pyplot.plot(specdata['Lambdas'],specdata['filter_trans'][:,2])
-    # plt.pyplot.show()
-    # pdb.set_trace()
+
     #remove duplicate laser lines,
     adjusted_lasers = adjusted_lasers.loc[:,~adjusted_lasers.columns.duplicated()].copy()
     paired_lasers[:,0] = adjusted_lasers.loc[:,exc_lines[0]].sum(1) #add the frist laser pair into one array
     paired_lasers[:,1] = adjusted_lasers.loc[:,exc_lines[1]].sum(1) #add the second laser pair into one array
 
-    #assemble paired filters (sum filter pairs)
-    paired_filters =  np.zeros((len(specdata['Lambdas']),len(specdata['filters'])))
-    paired_filters[:,0] = specdata['filter_trans'][:,[0,1]].sum(1) #sum first pair
-    paired_filters[:,1] = specdata['filter_trans'][:,[2,3]].sum(1) # sum second pair
+    #assemble paired filters 
+    #initalize wavelength by filter pair by 2 (# of pairs) matrix to store fitler transmission, and assemble filter pairs
+    paired_filters =  np.zeros((len(specdata['Lambdas']),len(specdata['filters']),len(specdata['filters'])))
+    paired_filters[:,[0,1],[0]] = specdata['filter_trans'][:,[0,1]]
+    paired_filters[:,[0,1],[1]] = specdata['filter_trans'][:,[2,3]]
     cameras = ['BSI','Andor'] 
     # plt.pyplot.plot(specdata['Lambdas'],paired_filters[:,0])
-    # plt.pyplot.show()
-    # Loop through the entries and populate
+    k=0
+    nRows = nCols = 4
+    dayRowCol = np.array([i + 1 for i in range(nRows * nCols)]).reshape(nRows, nCols)
+    fig1, ax1 = plt.pyplot.subplots(nrows=nRows,ncols=nCols,figsize=(20,8))
+    fig2, ax2 = plt.pyplot.subplots(nrows=nRows,ncols=nCols,figsize=(20,8))
     for m, exc_line in enumerate(exc_lines):
         for mp, em_filter in enumerate(specdata['filters']):
             for n, FP in enumerate(FPs):
+                k = k+1 #for subplots
+                rowIdx, colIdx = np.argwhere(dayRowCol == k)[0]
                 # Collect excitation terms
-                # laser_spec =  np.array(specdata['lasers'])[:,m]
                 laser_spec = np.array(paired_lasers[:,m])
                 abs_spec = np.array(specdata['EX_EM'][0,n])
                 ex_prod = (laser_spec  *  abs_spec)
@@ -47,51 +50,51 @@ def populate_matrix(specdata, exc_lines, laser_powers, exposure_times):
                 # Collect emission terms
                 em_spec = np.array(specdata['EX_EM'][1,n])
                 beam_split_trans = np.array(specdata['beam_split'])[:,mp] #not sure which cameras gets which beam yet
-                filter_em = np.array(paired_filters)[:,mp]
+                filter_em = np.array(paired_filters)[:,mp,m]
                 Quan_eff = np.array(specdata['QE_cameras'])[:,mp] #make sure this is correct camera order 
                 dichroic_trans = np.array(specdata['dichroic'])[:,1]
                 #constants
                 Quan_yield = np.array(specdata['QY'])[n]
                 t_exp = np.array(exposure_times)[m]
                 em_part = np.sum(em_spec * beam_split_trans * filter_em * Quan_eff * Quan_yield * t_exp * dichroic_trans)# Collect emission bits and sum/integrate
-                em_prod = (em_spec * beam_split_trans * filter_em * Quan_eff * Quan_yield * t_exp * dichroic_trans)
-                #plot resulting spectra
-            
-                # em = (em_spec * beam_split_trans * filter_em * Quan_eff * Quan_yield * t_exp * dichroic_trans)
-                # plt.pyplot.plot(specdata['Lambdas'],em)
-                # plt.pyplot.show()
-
-                # plt.pyplot.plot(specdata['Lambdas'],laser_spec)
-                # # plt.pyplot.plot(specdata['Lambdas'],dichroic_in)
-                # plt.pyplot.plot(specdata['Lambdas'],abs_spec)
-                # plt.pyplot.show()
-                # if n ==3 and m ==1 and mp ==1:
-                # plt.pyplot.plot(specdata['Lambdas'],laser_spec)
-                # plt.pyplot.plot(specdata['Lambdas'],abs_spec)
-                # ex_spec2 = np.array(specdata['EX_EM'][0,1])#
-                # plt.pyplot.plot(specdata['Lambdas'],ex_spec2)
-                # plt.pyplot.legend(('laser line','excitation spec'),loc='center left', bbox_to_anchor=(0, 1.1), ncol=3)
-                # plt.pyplot.xlabel('Wavelength')
-                # plt.pyplot.ylabel('Tramsmission') 
-                # plt.pyplot.show()                   
-                # em_spec2 = np.array(specdata['EX_EM'][1,1])#
-                plt.pyplot.plot(specdata['Lambdas'],em_spec)
-                plt.pyplot.plot(specdata['Lambdas'],beam_split_trans)
-                plt.pyplot.plot(specdata['Lambdas'],filter_em)
-                plt.pyplot.plot(specdata['Lambdas'],Quan_eff)
-                plt.pyplot.plot(specdata['Lambdas'],dichroic_trans)
-                # plt.pyplot.plot(specdata['Lambdas'],em_spec2)
-                plt.pyplot.fill_between(specdata['Lambdas'],em_prod)
-                # plt.pyplot.plot(specdata['Lambdas'],em_prod)
-                plt.pyplot.legend(('em','beam_splitter','filter','QE','dichroic','product'),loc='center left', bbox_to_anchor=(0, 1.1),ncol=3)
-                plt.pyplot.xlabel('Wavelength')
-                plt.pyplot.ylabel('Tramsmission')
-                print(exc_line, em_filter, FP,cameras[mp])      
-                plt.pyplot.show() 
-                #     pdb.set_trace()         
+                em_prod = (em_spec * beam_split_trans * filter_em * Quan_eff *  dichroic_trans) #for visualization
                 c_3d[m,mp,n] = exc_part * em_part
 
+                #plot ex spectra
+                axis2 = ax2[rowIdx, colIdx]
+                axis2.plot(specdata['Lambdas'],laser_spec)
+                axis2.plot(specdata['Lambdas'],abs_spec)
+                axis2.fill_between(specdata['Lambdas'],ex_prod)
+
+                #plot em spectra
+                axis1 = ax1[rowIdx, colIdx]
+                axis1.plot(specdata['Lambdas'],em_spec)
+                axis1.plot(specdata['Lambdas'],beam_split_trans)
+                axis1.plot(specdata['Lambdas'],filter_em)
+                axis1.plot(specdata['Lambdas'],Quan_eff)
+                axis1.plot(specdata['Lambdas'],dichroic_trans)
+                axis1.fill_between(specdata['Lambdas'],em_prod)
+                if k ==4:
+                    axis1.legend(('em','beam_splitter','filter','QE','dichroic','captured emission'),loc='center left', bbox_to_anchor=(1, 1.1),ncol=3)
+                    axis2.legend(('ex','laser pair'),loc='center left', bbox_to_anchor=(1, 1.1),ncol=3)                  
+                if k>=12:
+                    axis1.set_xlabel('Wavelength')
+                    axis2.set_xlabel('Wavelength')
+                if k%4 ==1:
+                    axis1.set_ylabel('Tramsmission')
+                    axis2.set_ylabel('Tramsmission')
+                print(exc_line, em_filter, FP,cameras[mp], c_3d[m,mp,n])   
+                axis1.set_title(FP + ' laser: ' + exc_line[mp] + '. ' + em_filter[m] + '\n' + cameras[mp] ,fontsize=8)      
+
     # Collapse over first two dimensions
+    fig1.tight_layout()
+    fig2.tight_layout()
+    # plt.pyplot.set_suptitle('Theoretical emission captured by Dragonfly', y = 1) 
+    fig1.suptitle('Theoretical emission captured by Dragonfly', y = 1) 
+    fig2.suptitle('Theoretical excitation spectra/laser lines', y = 1) 
+    fig1.savefig('./example_output_ex.pdf', bbox_inches=None, dpi=300,facecolor='white')
+    fig2.savefig('./example_output_em.pdf', bbox_inches=None, dpi=300,facecolor='white')
+    plt.pyplot.show()
     c_2d = np.reshape(c_3d, (c_3d.shape[0]*c_3d.shape[1], c_3d.shape[2]))
     
     return c_2d
--- util.py
+++ util.py
@@ -226,10 +226,8 @@ def get_filepaths(datafolder, **filters):
         print('using requested filters...',filters)
         filters = filters['filters']
     else:
-        # filters = ['TR-DFLY-F521-038' , 'TR-DFLY-F698-077'] #input filter names  
         print('using default fitlers...', filters)
-        # filters = [['TR-DFLY-F450-050','TR-DFLY-F600-050'],['TR-DFLY-F521-038','TR-DFLY-F698-077']] #input filter names  
-        filters = [['TR-DFLY-F450-050','TR-DFLY-F521-038'],['TR-DFLY-F600-050','TR-DFLY-F698-077']] #input filter names (paired filter for each camera)  
+        filters = [['TR-DFLY-F450-050','TR-DFLY-F600-050'],['TR-DFLY-F521-038','TR-DFLY-F698-077']] #input filter names  
 
     bs = ['TR-DFLY-CMDM-565'] #name of beam spliter 
     paths = {"bsi_path":bsi_path, "ixon_path":ixon_path,"laser_file":laser_file, "dichroic_file":dichroic_file,"filter_folder":filter_folder,"bs_folder":bs_folder,"filters":filters,"bs":bs}
